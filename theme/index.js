9e744fea68fedbfdaf2bb6048f9a73bd8fa18eef
Matheus Aurélio <matheusaurelio2004@gmail.com>
2022-08-23 23:48:04 -0300

    :sparkles: feat: Armazena tema escolhido no localstorage

--- theme/index.js
+++ theme/index.js
@@ -1,12 +1,34 @@
 const html = document.querySelector("html")
 const iconTheme = document.querySelector(".switch-theme span i")
 
-function changeTheme() {
-    const isLightTheme = html.getAttribute("data-theme") === "light"
-    isLightTheme ? html.setAttribute("data-theme", "dark") : html.setAttribute("data-theme", "light")
+window.addEventListener("load", () => {
+    getTheme()
+})
+
+function changeTheme(theme) {
+    html.classList.add(theme)
+    saveTheme(theme)
+}
+
+function saveTheme(theme) {
+    localStorage.setItem("theme", theme)
+}
+
+function checkTheme() {
+    const isLightTheme = html.classList.contains("light")
+    
+    isLightTheme ? (html.classList.add("dark"), html.classList.remove("light")) : (html.classList.add("light"), html.classList.remove("dark"))
+    const theme = html.className
+    saveTheme(theme)
 }
 
 iconTheme.onclick = () => {
     const isMoon = iconTheme.className === "fa-regular fa-moon"
-    isMoon ? (iconTheme.className = "fa-regular fa-sun", changeTheme()) : (iconTheme.className = "fa-regular fa-moon", changeTheme())
+    isMoon ? (iconTheme.className = "fa-regular fa-sun", checkTheme()) : (iconTheme.className = "fa-regular fa-moon", checkTheme())
+}
+
+function getTheme() {
+    const theme = localStorage.getItem("theme")
+
+    if (theme) changeTheme(theme)
 }
