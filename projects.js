92fdc8096b1ba7538f7c22cabb7a3228c763efbd
Matheus Aurélio <matheusaurelio2004@gmail.com>
2022-09-07 10:16:36 -0300

    :sparkles: feat: Adiciona link dos repositórios

--- projects.js
+++ projects.js
@@ -4,42 +4,42 @@ const details = [
         website: "https://weather-osuopckm8-matheus2004a.vercel.app/",
         repo: "Weather App",
         description: "Aplicação que exibe dados do clima de uma região pesquisada em tempo real.",
-        link: ""
+        link: "https://github.com/Matheus2004a/Weather-App"
     },
     {
         image: "https://raw.githubusercontent.com/Matheus2004a/ecommerce_products/master/design/desktop-preview.jpg",
         website: "https://ecommerce-products-5m70nh68a-matheus2004a.vercel.app/",
         repo: "Seção de ecommerce de produto",
         description: "Seção de descrição de um produto de um ecommerce fictício.",
-        link: ""
+        link: "https://github.com/Matheus2004a/ecommerce_products"
     },
     {
         image: "https://github.com/Matheus2004a/App-ToDo/raw/master/design/app-todo.png",
         website: "https://matheus2004a.github.io/App-ToDo/",
         repo: "App de tarefas",
         description: "Aplicação para fazer registro de tarefas a serem feitas, marcá - las como concluídas e sua excluí - las.",
-        link: ""
+        link: "https://github.com/Matheus2004a/App-ToDo"
     },
     {
         image: "https://github.com/Matheus2004a/Interactive-Pricing/raw/master/design/desktop-preview.jpg",
         website: "https://matheus2004a.github.io/Interactive-Pricing/",
         repo: "Preço Interativo",
         description: "Componente interativo onde é possível visualizar a mudança de valores conforme interação com a barra e o clique no botão toogle.",
-        link: ""
+        link: "https://github.com/Matheus2004a/Interactive-Pricing"
     },
     {
         image: "https://github.com/Matheus2004a/Calculator-App/raw/master/design/desktop-design-empty.jpg",
         website: "https://matheus2004a.github.io/Calculator-App/",
         repo: "Cálculo de gorjetas",
         description: "Aplicação que calcula o custo total de uma conta de acordo com a quantidade de pessoas e acrescido com o valor (%) da gorjeta.",
-        link: ""
+        link: "https://github.com/Matheus2004a/Calculator-App"
     },
     {
         image: "https://github.com/Matheus2004a/Signup-Form-Master/raw/master/design/desktop-preview.jpg",
         website: "https://matheus2004a.github.io/Signup-Form-Master/",
         repo: "Validação de formulário",
         description: "Formulário que contém validações individuais para cada campo de forma interativa e inclusive validação para e-mail.",
-        link: ""
+        link: "https://github.com/Matheus2004a/Signup-Form-Master"
     },
 ]
 
