e8271c1d5e745a7d01b0ef85eada0a058c376399
Matheus Aurélio <matheusaurelio2004@gmail.com>
2022-09-09 21:15:39 -0300

    :ok_hand: style: Remove suavidade da seção de projetos

--- libs/scroll-reveal.js
+++ libs/scroll-reveal.js
@@ -9,7 +9,6 @@ scrollReveal.reveal(`
     #about-me, #about-me section, 
     #services, #services h2, #services section .card,
     #abilities, #abilities h2,
-    #projects, #projects h2, #projects .slides .card,
     .education, .education h2`,
     { interval: 100 }
 )
